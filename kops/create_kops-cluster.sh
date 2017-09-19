59faddd746932859129a3e047c50dda4028f568f
aaf <alonsofonseca.angel@gmail.com>
2017-09-19 18:19:19 +0200

    kops with some tweaks

--- kops/create_kops-cluster.sh
+++ kops/create_kops-cluster.sh
@@ -3,23 +3,27 @@
 
 # Source our aws environment
 # needed in my case??
-SOURCE_ENV="./aws.conf"
-[[ -f ${SOURCE_ENV} ]] && . ${SOURCE_ENV}
+#SOURCE_ENV="./aws.conf"
+#[[ -f ${SOURCE_ENV} ]] && . ${SOURCE_ENV}
 
-export NAME="af-k8s.internal"
-export AWS_PROFILE=${AWSPROF}
+export NAME="af-k8s.fodpanda.com"
+export STACK="af-k8s"
+export DOMAIN="fodpanda.com"
+export AWS_PROFILE="test-k8s"
 export KOPS_STATE_STORE=s3://clusters.${STACK}.${DOMAIN}
 #export KOPS_FEATURE_FLAGS="+UseLegacyELBName"
 #export KOPS_FEATURE_FLAGS="+DrainAndValidateRollingUpdate"
 # needed?
 #export VPC_ID="vpc-xxxxxxxx"
-export NETWORK_CIDR="10.99.0.0/20"
+export NETWORK_CIDR="10.44.0.0/20"
 export ZONES="us-west-2a,us-west-2b,us-west-2c"
-export SSH_PUBLIC_KEY="~/.ssh/ec2key-pub.pem"
+export SSH_PUBLIC_KEY="~/.ssh/kops_rsa.pub"
 # needed?
 #export ADMIN_ACCESS="[${NETWORK_CIDR},210.10.195.106/32,123.243.200.245/32]"
-export DNS_ZONE_PRIVATE_ID="ZXXXXXXXXXXXY"
-export DNS_ZONE_ID="ZXXXXXXXXXXXXI"
+export DNS_ZONE_PRIVATE_ID="Z22J8RVEAKU7B7"
+
+# needed?
+#export DNS_ZONE_ID="ZXXXXXXXXXXXXI"
 export NODE_SIZE="t2.micro"
 export NODE_COUNT=6
 export MASTER_SIZE="t2.small"
@@ -33,6 +37,13 @@ create() {
   echo "You'll need to prepare:"
   echo " - subdomain delegation for ${STACK}.${DOMAIN}"
   echo " - S3 bucket named clusters.${STACK}.${DOMAIN}"
+  echo " - a VPC (get its ID in this script as a variable)"
+
+  # TODO:
+  #ERRORS:
+#error determining default DNS zone: No matching hosted zones found for ".af-k8s.internal"; please create one (e.g. "af-k8s.internal") first
+#./create_kops-cluster.sh: line 50: --dns: command not found
+#./create_kops-cluster.sh: line 58: --ssh-public-key: command not found
 
 kops create cluster \
     --name "${NAME}" \
@@ -44,26 +55,24 @@ kops create cluster \
     --zones "${ZONES}" \
     --master-zones "${ZONES}" \
     --dns-zone "${DNS_ZONE_PRIVATE_ID}" \
-    --dns private \
     --node-size "${NODE_SIZE}" \
+    --node-count "${NODE_COUNT}" \
     --master-size "${MASTER_SIZE}" \
+    --master-count "${MASTER_COUNT}" \
     --topology private \
     --network-cidr "${NETWORK_CIDR}" \
     --networking calico \
-    # needed?
-   # --vpc "${VPC_ID}" \
+    --bastion \
     --ssh-public-key ${SSH_PUBLIC_KEY}
-  kops create cluster \
-    --master-count 3 \
-    --node-count 3 \
-    --zones ${AZS} \
-    --node-size t2.micro \
-    --master-size t2.small \
-    kops.${STACK}.${DOMAIN}
+
+    # needed?
+    #--dns private \
+# needed if existing vpc
+   # --vpc "${VPC_ID}" \
 
    echo "NOTE:"
    echo "run the following instead to create your cluster:"
-   echo "export AWS_PROFILE=${AWSPROF}; export KOPS_STATE_STORE=s3://clusters.${STACK}.${DOMAIN}; kops update cluster kops.${STACK}.${DOMAIN} --yes"
+   echo "export AWS_PROFILE=${AWS_PROFILE}; export KOPS_STATE_STORE=s3://clusters.${STACK}.${DOMAIN}; kops update cluster kops.${STACK}.${DOMAIN} --yes"
 }
 
 create
